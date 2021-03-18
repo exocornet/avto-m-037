01f6b05bbde03cb45d17c2f968581378bc130b43
Evgeniy Sevostyanov <exocornet@gmail.com>
2021-03-18 09:29:02 +0300

    сборка проекта

---
+++ src/page.js
@@ -0,0 +1,21 @@
+import './scss/main.scss';
+// import './js/jquery.min';
+
+import * as $ from 'jquery';
+import "jquery-migrate";
+import "./js/popper.min";
+import "./js/bootstrap.min";
+import "./js/jquery.easing";
+import "./js/jquery.waypoints.min"; //install jquery-waypoints
+import "./js/jquery.stellar.min";
+import "./js/jquery.animateNumber.min";
+import "./js/bootstrap-datepicker";
+import "./js/jquery.timepicker.min";
+import "./js/owl.carousel.min";
+import "./js/jquery.magnific-popup.min";
+import "./js/scrollax.min";
+import "google-maps"; //из папки не подгружает ошибка на сайте
+import "./js/main";
+
+// import './js/jquery-3.2.1.min';
+// import 'maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false';
---
+++ webpack.config.js
@@ -0,0 +1,122 @@
+const path = require('path');
+const fs = require('fs');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+// const globImporter = require('node-sass-glob-importer');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/page.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'js/page.js'
+  },
+
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    stats: 'errors-only',
+    open: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: [
+          {
+            loader: "html-loader"
+          },
+          {
+            loader: "pug-html-loader",
+            options: {
+              "pretty": true
+            }
+          }
+        ]
+      },
+      {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader?url=false",
+          // "style-loader",
+          // "css-loader",
+          {
+            loader: "sass-loader",
+          }
+        ],
+      },
+
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //         loader: 'babel-loader',
+      //         options: {
+      //           presets: ['@babel/preset-env'],
+      //           plugins: ['@babel/plugin-proposal-class-properties']
+      //         }
+      //       }
+      // },
+
+      // {
+      //   test: /\.(png|jpg|gif)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name(file) {
+      //           if (process.env.NODE_ENV === 'development') {
+      //             return 'assets/images/[hash].[ext]';
+      //           }
+      //
+      //           return 'assets/images/[name].[ext]';
+      //         },
+      //       }
+      //     }
+      //   ]
+      // },
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: './src/pug/index.pug',
+      inject: 'body'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/main.css"
+    }),
+
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/public',
+          to: './'
+        }
+      ]
+    }),
+
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      // jquery: "./src/js/jquery.min.js",
+      // jQuery: "./src/js/jquery.min.js",
+      // "window.jQuery": "./src/js/jquery.min.js"
+    })
+
+  ]
+}
