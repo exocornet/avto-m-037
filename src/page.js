598b31553ae4a653fea0f535eed0bfc6862bf4cd
Evgeniy Sevostyanov <exocornet@gmail.com>
2021-05-13 18:05:41 +0300

    fix

--- src/page.js
+++ src/page.js
@@ -10,15 +10,16 @@ import "./js/popper.min";
 import "./js/bootstrap.min";
 import "./js/jquery.easing";
 import "./js/jquery.waypoints.min"; //install jquery-waypoints
-// import "./js/jquery.stellar.min";
+// import "./js/jquery.stellar.min"; паралах на удаление
 import "./js/jquery.animateNumber.min";
 import "./js/bootstrap-datepicker";
 import "./js/jquery.timepicker.min";
 import "./js/owl.carousel.min";
 import "./js/jquery.magnific-popup.min";
 import "./js/scrollax.min";
-import "google-maps"; //из папки не подгружает ошибка на сайте
+// import "google-maps"; //из папки не подгружает ошибка на сайте
 import "./js/main";
 
-// import './js/jquery-3.2.1.min';
+// import './js/jquery-3.2.1.min'; ненужен
 // import 'maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false';
+
